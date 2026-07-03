2b58eb9eed9b069dbf0c831afba2031a1752d57f
FRAN4602 <falbarracin@frba.utn.edu.ar>
2026-07-03 02:13:24 -0300

    Commit inicial

---
+++ src/main/java/ar/edu/utn/ba/ddsi/clima_alert/ClimaAlertApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.clima_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/ba/ddsi/clima_alert/ClimaAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.clima_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
